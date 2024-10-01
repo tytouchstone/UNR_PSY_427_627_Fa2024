54d51ecb91081c4a5d53dd4c4fd4097384180a0c
Mark Lescroart <mlescroart@unr.edu>
2024-10-01 13:33:31 -0700

    Lecture 9 timing

---
+++ Experiment1/timingFunctionExamples.m
@@ -0,0 +1,16 @@
+%Time function
+
+%% Get time on current clock, since computer startup
+t0 = GetSecs; 
+
+%% Wait a particular duration 
+t1 = GetSecs;
+WaitSecs(3);
+t2 = GetSecs;
+disp(t2-t1)
+
+%% Wait until a particular time
+t3 = GetSecs;
+WaitSecs('untiltime', t3+2)
+t4 = GetSecs;
+disp(t4-t3)
